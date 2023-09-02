ae32a411c33608c0d009f56a6d8ba6901f2acee4
lexur <lostsunset69@gmail.com>
2023-09-02 13:15:58 +0800

    检测软件是否安装

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+app=neovim
+
+if dpkg -s $app >/dev/null 2>&1; then
+    echo "$app 已安装"
+else
+    echo "$app 未安装，正在安装..."
+    apt-get install $app
+fi
+
 # 软链接当前路径到指定目录
 ln -s "$(pwd)" ~/.config/
 
